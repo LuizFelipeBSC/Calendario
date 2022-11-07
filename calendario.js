1f86c96c2d6a76859f6614a303a0c1c157670961
Victor Dias <62574942+ViiDias@users.noreply.github.com>
2022-11-07 10:35:17 -0300

    Calendario
    Correção Background

--- calendario.js
+++ calendario.js
@@ -1,5 +1,5 @@
-//const meses = [ 'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
-//const dias = [ 'Segunda-Feira', 'Terça-Feira', 'Quarta-Feira', 'Quinta-Feira', 'Sexta-feira', 'Sabado', 'Domingo'];
+const meses = [ 'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+const dias = [ 'Segunda-Feira', 'Terça-Feira', 'Quarta-Feira', 'Quinta-Feira', 'Sexta-feira', 'Sabado', 'Domingo'];
 
 
 //if ((ano % 4 == 0) && ((ano % 100 != 0) || (ano % 400 == 0))
@@ -9,41 +9,41 @@ function daysOfmonth(month, ano) {
 
     var data = new Date(ano, month, 0);
     var mes = d.getMonth() + 1;
-    var days = [];
+    var days;
 
     if (ano == null && month == null) {
-        days[d.getMonth()] = new Date(d.getFullYear() ,mes, 0).getDate();
-        return days;
+        days = new Date(d.getFullYear() ,mes, 0).getDate();
         
+        return montarAgenda(days);
+
     } else if (month != null) {
-        days[month] = new Date(d.getFullYear() ,month, 0).getDate();
+        days = new Date(d.getFullYear() ,month, 0).getDate();
 
-        return days;
+        return montarAgenda(days);
     }
 
-    for (index = 1; index <=12 ; index++) {
+    //for (index = 1; index <=12 ; index++) {
             
-        days[index] = new Date(d.getFullYear() ,index, 0).getDate();
-    }
+    //    days[index] = new Date(d.getFullYear() ,index, 0).getDate();
+    //}
    
     return days();
 }
 
-function calendario() {
-    document.write('<tr>');
-    document.write('<th> Domingo</th>');
-    document.write('<th> Segunda-Feira</th>');
-    document.write('<th> Terça-Feira</th>');
-    document.write('<th> Quarta-Feira</th>');
-    document.write('<th> Quinta-Feira</th>');
-    document.write('<th> Sábado</th>');
-    document.write('</tr>');
-}
+function montarAgenda(days) {
+    var array1 = [];
 
-function postMessage() {
+    for (i = 1; i <= days; i++) {
+
+        array1[i] = i;
+        
+    }
+
+    var listItems = array1.map(function(city){
+      return '<td>' + city + '</td>';
+    })
+
+    document.getElementById('agenda').innerHTML = listItems.join('');
+    document.getElementById('agenda').innerHTML = listItems.join('');
     
 }
-
-// Exemplo:
-    console.log(daysOfmonth()); // Exibe 28. 
-
